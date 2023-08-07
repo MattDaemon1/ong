5bc9b4712a247bdef6b616c46d3427b01a315b5e
MattDaemon1 <matt@mattkonnect.com>
2023-08-07 18:21:44 +0200

    ajout section hero, à propos, projets, témoignages

--- index.php
+++ index.php
@@ -2,18 +2,52 @@
 <?php include __DIR__ . '/includes/nav.php'; ?>
 <?php
 $pageTitle = 'Accueil';
-include 'header.php';
+include __DIR__ . '/includes/header.php';
 ?>
 <main>
-    <div class="container">
-        <h1>ONG - Bienvenue sur notre site</h1>
-    </div>
-    <section class="projects">
-        <!-- Vous pouvez ajouter ici le contenu de la section "projects" ou le remplir ultérieurement. -->
+    <!-- Hero Section avec image de fond et bouton "Faire un don" -->
+    <section class="hero-section" style="background-image: url('./assets/img/paysage2.jpg');">
+        <div class="container">
+            <h1>Bienvenue sur ONG Allo la Terre</h1>
+            <p>Rejoignez-nous dans notre mission pour aider ceux qui en ont le plus besoin.</p>
+        </div>
+        <div class="container d-flex justify-content-center">
+            <a href="projets.php" class="btn btn-primary">Faire un don</a>
+        </div>
     </section>
 
-    <section class="testimonials">
-        <!-- Vous pouvez ajouter ici le contenu de la section "testimonials" ou le remplir ultérieurement. -->
+    <!-- Section À Propos -->
+    <section class="about-section">
+        <div class="container">
+            <h2>À Propos</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum lacus sed tellus viverra placerat.</p>
+        </div>
+        <div class="container d-flex justify-content-center">
+            <a href="about.php" class="btn btn-primary">En savoir plus</a>
+        </div>
+    </section>
+
+    <!-- Section Projets -->
+    <section class="projects-section">
+        <div class="container">
+            <h2>Derniers Projets</h2>
+            <div class="row">
+                <!-- Ici cartes de projets -->
+            </div>
+            <div class="container d-flex justify-content-center">
+            <a href="projets.php" class="btn btn-primary">Voir tous les projets</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Section Témoignages -->
+    <section class="testimonials-section">
+        <div class="container">
+            <h2>Témoignages</h2>
+            <div class="row">
+                <!-- Ici cartes de témoignages -->
+            </div>
+        </div>
     </section>
 </main>
 <?php include __DIR__ . '/includes/footer.php'; ?>
