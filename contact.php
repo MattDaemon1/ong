b1050aaf22fd7dacc4701786265b4d4cdae925be
MattDaemon1 <matt@mattkonnect.com>
2023-08-08 14:04:22 +0200

    ajout formulaire de contact

--- contact.php
+++ contact.php
@@ -24,7 +24,27 @@ include __DIR__ . '/includes/header.php';
             <!-- Formulaire de contact -->
             <div class="col-lg-6">
                 <form id="contactForm">
-                    <!-- ... (le reste du formulaire) -->
+                    <div class="form-group">
+                        <label for="firstName">Prénom:</label>
+                        <input type="text" class="form-control" id="firstName" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="lastName">Nom:</label>
+                        <input type="text" class="form-control" id="lastName" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" class="form-control" id="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="message">Message:</label>
+                        <textarea class="form-control" id="message" rows="4" required></textarea>
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="rgpd" required>
+                        <label class="form-check-label" for="rgpd">J'accepte la politique de confidentialité.</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Envoyer</button>
                 </form>
                 <div id="successMessage" class="alert alert-success mt-4" style="display: none;">Votre message a été envoyé avec succès!</div>
                 <div id="errorMessage" class="alert alert-danger mt-4" style="display: none;">Veuillez remplir tous les champs correctement.</div>
